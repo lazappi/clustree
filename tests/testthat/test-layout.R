c9e2ec1d9184ce93613d05101dedb90a62cd4fdf
Luke Zappia <lazappi@users.noreply.github.com>
2018-05-22 16:26:02 +1000

    Fix layout test

--- tests/testthat/test-layout.R
+++ tests/testthat/test-layout.R
@@ -3,7 +3,7 @@ context("layout")
 data("iris_clusts")
 
 test_that("non-core layout works", {
-    expect_is(clustree(iris_clusts, prefix = "K", use_core_network = FALSE),
+    expect_is(clustree(iris_clusts, prefix = "K", use_core_edges = FALSE),
               c("gg", "ggplot"))
 })
 
