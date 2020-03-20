08d7c910f6585c1e5b5aa613d1ae4f63c4da5463
Luke Zappia <lazappi@users.noreply.github.com>
2020-03-20 10:20:19 +0100

    Add feature name check to SCE interface

--- R/clustree.R
+++ R/clustree.R
@@ -398,14 +398,22 @@ clustree.SingleCellExperiment <- function(x, prefix, exprs = "counts", ...) {
     }
 
     args <- list(...)
-    for (node_aes in c("node_colour", "node_size", "node_alpha")) {
-        if (node_aes %in% names(args)) {
-            node_aes_value <- args[[node_aes]]
-            if (node_aes_value %in% rownames(x)) {
-                aes_name <- paste0(exprs, "_", node_aes_value)
-                x@colData[aes_name] <- exprs_mat[node_aes_value, ]
-                args[[node_aes]] <- aes_name
+    node_aes_sel <- c("node_colour", "node_size", "node_alpha")
+    node_aes_sel <- node_aes_sel[node_aes_sel %in% names(args)]
+    for (node_aes in node_aes_sel) {
+        node_aes_value <- args[[node_aes]]
+        if (node_aes_value %in% rownames(x)) {
+            node_aes_name <- make.names(node_aes_value)
+            if (node_aes_value != node_aes_name) {
+                warning(
+                    "The feature name ", node_aes_value,
+                    " will be converted to ", node_aes_name,
+                    call. = FALSE
+                )
             }
+            aes_name <- paste0(exprs, "_", node_aes_name)
+            x@colData[aes_name] <- exprs_mat[node_aes_value, ]
+            args[[node_aes]] <- aes_name
         }
     }
 
