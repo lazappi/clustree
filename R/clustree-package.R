682cfa365f5c59a2a21ee9dc3eb9d7384dad607a
Luke Zappia <lazappi@users.noreply.github.com>
2020-12-30 13:55:38 +0100

    Use usethis package doc file

--- R/clustree-package.R
+++ R/clustree-package.R
@@ -1,12 +1,8 @@
-#' Clustree
-#'
-#' Deciding what resolution to use can be a difficult question when
-#' approaching a clustering analysis. One way to approach this problem is to
-#' look at how samples move as the number of clusters increases. This package
-#' allows you to produce clustering trees, a visualisation for interrogating
-#' clusterings as resolution increases.
-#'
-#' @name clustree-package
-#' @docType package
-#' @rdname clustree-package
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
 NULL
