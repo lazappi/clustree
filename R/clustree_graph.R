a1659312a0265c080a690c2beea60872b75251f1
Luke Zappia <lazappi@users.noreply.github.com>
2021-01-05 16:06:20 +0100

    Add clustree_graph class
    Extension of tbl_graph that stores a sample_metadata table

---
+++ R/clustree_graph.R
@@ -0,0 +1,141 @@
+#' Data structure for manipulating clustering trees
+#'
+#' The `clustree_graph` class is an extension to the `tbl_graph` class which
+#' adds a table for storing metadata about individual samples. Unlike most
+#' graphs where is node is a sample, nodes in a clustering tree represent
+#' clusters of multiple samples. Storing metadata about the original samples
+#' makes it possible to calculate additional node (or edge) level summaries as
+#' required rather than having to compute these when the graph is created.
+#'
+#' @param ... Arguments passed to [tidygraph::tbl_graph()] or conversion
+#' functions
+#' @param metadata A `data.frame` providing information about individual
+#' samples. If a column named `.clustree_idx` is not already present it will
+#' be created using the row names. If `NULL` an empty `tibble` is created.
+#'
+#' @details
+#' A `clustree_graph` object should usually be created using the
+#' [build_clustree_graph()] function. If using [clustree_graph()] directly then
+#' both the `nodes` and `edges` tables must include a column named
+#' "`.clustree_indices`" which is a list of vectors giving the indices in
+#' the `.clustree_idx` column of `metadata` matching the samples associated
+#' with each node/edge.
+#'
+#' The metadata table is stored as a graph attribute accessible with
+#' [igraph::graph_attr()].
+#'
+#' @seealso [tidygraph::tbl_graph()] for the `tbl_graph` object and
+#' [build_clustree_graph()] for building `clustree_graph` objects from various
+#' inputs
+#'
+#' @return A `clustree_graph` object
+#' @export
+#'
+#' @examples
+#' nodes <- tibble::tibble(
+#'     node = c("A", "B"),
+#'     .clustree_indices = list(c("1", "2"), c("1"))
+#' )
+#'
+#' edges <- tibble::tibble(
+#'     from = "A",
+#'     to   = "B",
+#'     .clustree_indices = list(c("1"))
+#' )
+#'
+#' metadata <- data.frame(
+#'     .clustree_idx = c("1", "2"),
+#'     value = c("Example1", "Example2")
+#' )
+#'
+#' clustree_graph(nodes = nodes, edges = edges, metadata = metadata)
+clustree_graph <- function(..., metadata = NULL) {
+    tbl_graph <- tidygraph::tbl_graph(...)
+
+    tbl_graph <- tidygraph::activate(tbl_graph, "edges")
+    if (!(".clustree_indices" %in% colnames(tibble::as_tibble(tbl_graph)))) {
+        abort("edges do not contain a '.clustree_indices' column")
+    }
+
+    tbl_graph <- tidygraph::activate(tbl_graph, "nodes")
+    if (!(".clustree_indices" %in% colnames(tibble::as_tibble(tbl_graph)))) {
+        abort("nodes do not contain a '.clustree_indices' column")
+    }
+
+    if (!is.null(metadata) && !(".clustree_idx") %in% colnames(metadata)) {
+        inform("Adding '.clustree_idx' column to metadata using row names")
+        metadata[, ".clustree_idx"] <- rownames(metadata)
+    }
+
+    tbl_graph <- igraph::set_graph_attr(tbl_graph, ".clustree_metadata",
+                                        metadata)
+    as_clustree_graph(tbl_graph)
+}
+
+#' @rdname clustree_graph
+#' @export
+as_clustree_graph <- function(x, ..., metadata = NULL) {
+    UseMethod('as_clustree_graph')
+}
+
+#' @describeIn clustree_graph Default method. Tries to call
+#' [tidygraph::as_tbl_graph()] on the input before converting.
+#' @export
+as_clustree_graph.default <- function(x, ..., metadata = NULL) {
+    tryCatch({
+        as_clustree_graph(tidygraph::as_tbl_graph(x, ...))
+    }, error = function(err) {
+        abort(paste0("No support for ", class(x)[1], " objects'"))
+    })
+}
+
+#' @describeIn clustree_graph Method for `tbl_graph` objects. Stores `metadata`
+#' and appends the class.
+#' @export
+as_clustree_graph.tbl_graph <- function(x, ..., metadata = NULL) {
+
+    if (is.null(metadata)) {
+        metadata <- tibble::tibble(.clustree_index = character())
+    } else {
+        metadata <- tibble::as_tibble(metadata)
+    }
+
+    x <- igraph::set_graph_attr(x, ".clustree_metadata", metadata)
+
+    class(x) <- c("clustree_graph", "tbl_graph", "igraph")
+
+    return(x)
+}
+
+#' @rdname clustree_graph
+#' @export
+is.clustree_graph <- function(x) {
+    inherits(x, "clustree_graph")
+}
+
+#' @export
+print.clustree_graph <- function(x, ...) {
+    arg_list <- list(...)
+
+    cat_subtle("# A clustree_graph:\n")
+    cat_subtle("#\n")
+
+    cat_subtle("# --------------\n")
+    cat_subtle("#\n")
+    NextMethod()
+    cat_subtle("#\n")
+    cat_subtle("# --------------\n")
+
+    metadata <- igraph::graph_attr(x, ".clustree_metadata")
+    trunc <- do.call(
+        tibble::trunc_mat,
+        modifyList(arg_list, list(x = metadata, n = 6))
+    )
+    names(trunc$summary)[1] <- "Metadata"
+
+    cat_subtle("#\n")
+    print(trunc)
+
+    invisible(x)
+}
+
---
+++ R/summarise_metadata.R
@@ -0,0 +1,72 @@
+#' Summarise clustering tree metadata
+#'
+#' Tidyverse style function for summarising sample metadata by node or edge for
+#' a `clustree_graph` object.
+#'
+#' `summarise_metadata` and `summarize_metadata` are synonyms.
+#'
+#' @param .graph A `clustree_graph` object
+#' @param ... Arguments based to [dplyr::summarise()]
+#'
+#' @details
+#' This function is designed to make it easy to add additional summarised
+#' information to nodes or edges in as flexible a way as possible. It uses the
+#' following steps:
+#'
+#' 1. Extract nodes/edges (whichever are active) as a `tibble` using
+#' [tibble::as_tibble()]
+#' 2. Unnest the "`.clustree_indices`" column using [tidyr::unnest()]
+#' 3. Join the metadata table by the indices using [dplyr::left_join()]
+#' 4. Calculated summaries using [dplyr::summarise()]
+#' 5. Join the results to the original nodes/edges using [dplyr::left_join()]
+#'
+#' @seealso [dplyr::summarise()] for details on summarisation and [clustree_graph()] for `clustree_graph` objects
+#'
+#' @return A `clustree_graph` object
+#' @export
+#'
+#' @examples
+#' graph <- build_clustree_graph(nba_clusts)
+#'
+#' # Summarise by node
+#' graph <- tidygraph::activate(graph, "nodes")
+#' summarise_metadata(graph, MeanReboundPct = mean(ReboundPct))
+#'
+#' # Summarise by edge
+#' graph <- tidygraph::activate(graph, "edges")
+#' summarise_metadata(graph, MeanReboundPct = mean(ReboundPct))
+summarise_metadata <- function(.graph, ...) {
+    UseMethod("metadata")
+}
+#' @rdname summarise_metadata
+#' @export
+summarize_metadata <- summarise_metadata
+
+#' @rdname summarise_metadata
+#' @export
+summarise_metadata.clustree_graph <- function(.graph, ...) {
+
+    active <- attr(.graph, "active")
+
+    nested_tbl <- tibble::as_tibble(.graph)
+    metadata   <- igraph::graph_attr(.graph, ".clustree_metadata")
+
+    tbl <- tidyr::unnest(nested_tbl, .data$.clustree_indices)
+    tbl <- dplyr::left_join(
+        tbl,
+        metadata,
+        by = c(".clustree_indicies" = ".clustree_idx")
+    )
+
+    tbl <- switch (active,
+                   nodes = dplyr::group_by(tbl, .data$node),
+                   edges = dplyr::group_by(tbl, .data$from, .data$to)
+    )
+
+    summarised <- dplyr::summarise(tbl, ...)
+
+    switch (active,
+            nodes = tidygraph::left_join(.graph, summarised, by = "node"),
+            edges = tidygraph::left_join(.graph, summarised, by = c("from", "to"))
+    )
+}
